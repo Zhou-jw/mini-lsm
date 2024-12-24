cc2adb283dcde32e3a9a4c0b865e7e336a906902
zjw <zjw1858@163.com>
2024-12-24 13:55:20 +0800

    add w2d6 Task 1: WAL Encoding

--- mini-lsm-starter/src/wal.rs
+++ mini-lsm-starter/src/wal.rs
@@ -1,12 +1,12 @@
 #![allow(dead_code)] // REMOVE THIS LINE after fully implementing this functionality
 
-use std::fs::File;
-use std::io::BufWriter;
+use std::fs::{File, OpenOptions};
+use std::io::{BufWriter, Read, Write};
 use std::path::Path;
 use std::sync::Arc;
 
-use anyhow::Result;
-use bytes::Bytes;
+use anyhow::{Context, Result};
+use bytes::{Buf, BufMut, Bytes};
 use crossbeam_skiplist::SkipMap;
 use parking_lot::Mutex;
 
@@ -15,16 +15,51 @@ pub struct Wal {
 }
 
 impl Wal {
-    pub fn create(_path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+    pub fn create(path: impl AsRef<Path>) -> Result<Self> {
+        // let path = &_path;
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(path)
+            .context("fail to create WAL!")?;
+        Ok(Self {
+            file: Arc::new(Mutex::new(BufWriter::new(file))),
+        })
     }
 
-    pub fn recover(_path: impl AsRef<Path>, _skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
-        unimplemented!()
+    pub fn recover(path: impl AsRef<Path>, skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
+        let mut file = OpenOptions::new().read(true).append(true).open(path)?;
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+        let mut rbuf: &[u8] = buf.as_slice();
+        while rbuf.has_remaining() {
+            let key_len = rbuf.get_u16() as usize;
+            let key = Bytes::copy_from_slice(&rbuf[..key_len]);
+            rbuf.advance(key_len);
+            let value_len = rbuf.get_u16() as usize;
+            let value = Bytes::copy_from_slice(&rbuf[..value_len]);
+            rbuf.advance(value_len);
+            skiplist.insert(key, value);
+        }
+
+        Ok(Self {
+            file: Arc::new(Mutex::new(BufWriter::new(file))),
+        })
     }
 
-    pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+    pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        let mut file = self.file.lock();
+        let mut buf = Vec::new();
+        let estimated_size = 2 + key.len() + 2 + value.len();
+        buf.reserve(estimated_size);
+        buf.put_u16(key.len() as u16);
+        buf.put_slice(key);
+        buf.put_u16(value.len() as u16);
+        buf.put_slice(value);
+        file.write_all(buf.as_slice())
+            .context("fail to write to WAL")?;
+        Ok(())
     }
 
     /// Implement this in week 3, day 5.
@@ -33,6 +68,9 @@ impl Wal {
     }
 
     pub fn sync(&self) -> Result<()> {
-        unimplemented!()
+        let mut file = self.file.lock();
+        file.flush()?;
+        file.get_mut().sync_all()?;
+        Ok(())
     }
 }
