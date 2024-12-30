68f5e2f0cd3ce48ffadd9ba4d7c06d78ab29d110
zjw <zjw1858@163.com>
2024-12-31 01:34:00 +0800

    add w2d3 Task 1: Universal Compaction

--- mini-lsm-starter/src/compact/tiered.rs
+++ mini-lsm-starter/src/compact/tiered.rs
@@ -30,31 +30,70 @@ impl TieredCompactionController {
         &self,
         snapshot: &LsmStorageState,
     ) -> Option<TieredCompactionTask> {
-        assert!(snapshot.l0_sstables.is_empty(), "should not add l0 ssts in tiered compaction");
+        assert!(
+            snapshot.l0_sstables.is_empty(),
+            "should not add l0 ssts in tiered compaction"
+        );
 
         if snapshot.levels.len() <= self.options.num_tiers {
             return None;
         }
 
         // trigger by space amplification ratio
-        let mut all_size_except_last_level:usize = 0;
-        let last_level_size:usize = snapshot.levels.last().unwrap().1.len();
-        for idx in 0..snapshot.levels.len()-1 {
+        let mut all_size_except_last_level: usize = 0;
+        let last_level_size: usize = snapshot.levels.last().unwrap().1.len();
+        for idx in 0..snapshot.levels.len() - 1 {
             all_size_except_last_level = snapshot.levels[idx].1.len();
         }
-        let space_amplification_ratio = (all_size_except_last_level as f64) / (last_level_size as f64) * 100.0 ;
-        if space_amplification_ratio >= self.options.max_size_amplification_percent as f64{
-            let task = TieredCompactionTask{
+        let space_amplification_ratio =
+            (all_size_except_last_level as f64) / (last_level_size as f64) * 100.0;
+        if space_amplification_ratio >= self.options.max_size_amplification_percent as f64 {
+            let task = TieredCompactionTask {
                 tiers: snapshot.levels.clone(),
                 bottom_tier_included: true,
             };
-            println!("compaction triggered by space amplification ratio: {:?}", space_amplification_ratio);
+            println!(
+                "compaction triggered by space amplification ratio: {:?}",
+                space_amplification_ratio
+            );
             return Some(task);
         }
 
         //trigger by size ratio
-        
-        unimplemented!()
+        let size_ratio_trigger = (100.0 + self.options.size_ratio as f64) / 100.0;
+        let mut size_previous_tiers: usize = snapshot.levels.first().unwrap().1.len();
+        for idx in 1..snapshot.levels.len() {
+            let size_current_iter = snapshot.levels[idx].1.len();
+            let cur_size_ratio = size_current_iter as f64 / size_previous_tiers as f64 * 100.0;
+            if cur_size_ratio >= size_ratio_trigger && idx + 1 >= self.options.min_merge_width {
+                let task = TieredCompactionTask {
+                    tiers: snapshot
+                        .levels
+                        .iter()
+                        .take(idx)
+                        .cloned()
+                        .collect::<Vec<(usize, Vec<usize>)>>(),
+                    bottom_tier_included: idx + 1 == snapshot.levels.len(), // TODO: idx+1 >= snapshot.levels.len()
+                };
+                return Some(task);
+            }
+            size_previous_tiers += snapshot.levels[idx].1.len();
+        }
+
+        //none of triggers produce tasks, do a compaction to reduce the number of tiers, take all tiers into one
+        let num_iters_to_take = snapshot
+            .levels
+            .len()
+            .min(self.options.max_merge_width.unwrap_or(usize::MAX));
+        Some(TieredCompactionTask {
+            tiers: snapshot
+                .levels
+                .iter()
+                .take(num_iters_to_take)
+                .cloned()
+                .collect::<Vec<_>>(),
+            bottom_tier_included: true,
+        })
     }
 
     pub fn apply_compaction_result(
