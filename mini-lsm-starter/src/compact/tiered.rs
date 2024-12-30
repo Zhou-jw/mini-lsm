06cd9557680010dbd33372b4ea5c665e8af785b1
zjw <zjw1858@163.com>
2024-12-30 23:48:00 +0800

    add w2d3 Task 1.1: Triggered by Space Amplification Ratio

--- mini-lsm-starter/src/compact/tiered.rs
+++ mini-lsm-starter/src/compact/tiered.rs
@@ -27,8 +27,32 @@ impl TieredCompactionController {
 
     pub fn generate_compaction_task(
         &self,
-        _snapshot: &LsmStorageState,
+        snapshot: &LsmStorageState,
     ) -> Option<TieredCompactionTask> {
+        assert!(snapshot.l0_sstables.is_empty(), "should not add l0 ssts in tiered compaction");
+
+        if snapshot.levels.len() <= self.options.num_tiers {
+            return None;
+        }
+
+        // trigger by space amplification ratio
+        let mut all_size_except_last_level:usize = 0;
+        let last_level_size:usize = snapshot.levels.last().unwrap().1.len();
+        for idx in 0..snapshot.levels.len()-1 {
+            all_size_except_last_level = snapshot.levels[idx].1.len();
+        }
+        let space_amplification_ratio = (all_size_except_last_level as f64) / (last_level_size as f64) * 100.0 ;
+        if space_amplification_ratio >= self.options.max_size_amplification_percent as f64{
+            let task = TieredCompactionTask{
+                tiers: snapshot.levels.clone(),
+                bottom_tier_included: true,
+            };
+            println!("compaction triggered by space amplification ratio: {:?}", space_amplification_ratio);
+            return Some(task);
+        }
+
+        //trigger by size ratio
+        
         unimplemented!()
     }
 
