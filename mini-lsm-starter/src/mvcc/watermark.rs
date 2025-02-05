3650ee582e6d3ad9bcaee9c1ced196f9611adc9d
zjw <zjw1858@gmail.com>
2025-02-05 11:04:53 +0800

    add w3d4 Task 1: Implement Watermark

--- mini-lsm-starter/src/mvcc/watermark.rs
+++ mini-lsm-starter/src/mvcc/watermark.rs
@@ -14,11 +14,24 @@ impl Watermark {
         }
     }
 
-    pub fn add_reader(&mut self, ts: u64) {}
+    pub fn add_reader(&mut self, ts: u64) {
+        *self.readers.entry(ts).or_default() += 1;
+    }
 
-    pub fn remove_reader(&mut self, ts: u64) {}
+    pub fn remove_reader(&mut self, ts: u64) {
+        let cnt = self.readers.get_mut(&ts).unwrap();
+        *cnt -= 1;
+        if *cnt == 0 {
+            self.readers.remove(&ts);
+        }
+    }
 
     pub fn watermark(&self) -> Option<u64> {
-        Some(0)
+        if let Some(min_ts) = self.readers.keys().next() {
+            return Some(*min_ts);
+        }
+        else {
+            return None;
+        }
     }
 }
